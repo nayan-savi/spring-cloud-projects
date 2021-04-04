542107fb918e0c2328688895bb8dfdc8bbb0b41b
nayan.savi <nayan.savi@gmail.com>
2021-04-04 17:57:51 +0530

    initial commit

---
+++ spring-cloud-config-server-github/src/main/java/com/spring/cloud/configgithub/springcloudconfigservergithub/SpringCloudConfigServerGithubApplication.java
@@ -0,0 +1,13 @@
+package com.spring.cloud.configgithub.springcloudconfigservergithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigServerGithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigServerGithubApplication.class, args);
+	}
+
+}
---
+++ spring-cloud-config-server-github/src/test/java/com/spring/cloud/configgithub/springcloudconfigservergithub/SpringCloudConfigServerGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.cloud.configgithub.springcloudconfigservergithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerGithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
